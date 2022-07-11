1c76c9abff76ac5c3997b49742587414295ec6ff
0-th <artemisfowlx@protonmail.com>
2022-07-11 22:44:07 +0100

    feat: book object canbe created from form-field input, clear form-fields after submit event

--- app.js
+++ app.js
@@ -43,12 +43,39 @@ class UI {
 
         list.appendChild(row);
     }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 }
 
 // Store class: Handles storage
 
 // Event: display book
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
 // Event: to add a book
+form = document.querySelector('#book-form');
+form.addEventListener('submit',
+    (e) => {
+    //prevent default submit event
+    e.preventDefault();
+
+    //collect information from form fields
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //create Book object with info from form fields
+    const book = new Book(title, author, isbn);
+
+    //add book to UI
+    UI.addBookToList(book);
+
+    //clear fields after event
+    UI.clearFields();
+    });
 
 // Event: remove a book
